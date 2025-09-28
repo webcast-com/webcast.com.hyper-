803721f83f72dee77fca2d1648bf96d44cedd780
webcast-com <webcast.com@outlook.com>
2025-09-28 04:04:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -98,5 +98,6 @@ app.get('/api/posts', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Running on port ${PORT}`));
+
 
